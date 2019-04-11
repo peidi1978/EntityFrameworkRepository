1e8f4514dec214bb0fd64cde852fbe349cc46397
peidi1978 <peidi@hotmail.com>
2019-04-10 22:17:33 -0700

    adding unit tests.

---
+++ EntityFrameworkRepositoryTests/EntityFrameworkRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntityFrameworkRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkRepository.Tests
+{
+    [TestClass()]
+    public class EntityFrameworkRepositoryTests
+    {
+        [TestMethod()]
+        public void RunQueryTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
